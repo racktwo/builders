b5c4cfc23cafe168d9d298945ee58de5cf430a3f
racktwo <64808631+racktwo@users.noreply.github.com>
2020-05-12 03:10:12 -0300

    Update init.py

--- init.py
+++ init.py
@@ -1,1 +1,22 @@
-import zipfly
+    import zipfly
+    
+    paths = [ 
+        {
+            'fs': 'home/user/Videos/jupiter.mp4', 
+            'n': 'movies/jupiter.mp4', 
+        },       
+        {
+            'fs': 'home/user/Documents/mercury.mp4', 
+            'n': 'movies/mercury.mp4', 
+        },          
+    ]
+
+    zfly = zipfly.ZipFly( paths = paths )
+
+    generator = zfly.generator()
+    print ( generator )
+    # <generator object generator at 0x7f85aad60b13>
+
+    with open("test.zip", "wb") as f:
+        for i in generator:
+            f.write(i)
